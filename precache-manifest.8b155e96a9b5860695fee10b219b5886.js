e4ef952e901a2829b59a0f3b0d62a51a338c1cf6
Cloud Soh <cloud.soh@Clouds-MacBook-Pro.local>
2019-09-05 16:36:34 +0800

    Updates

---
+++ precache-manifest.8b155e96a9b5860695fee10b219b5886.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7d8016cb0f1d247e89ed58683a76a6",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "e3bdb7a8634048fa1163",
+    "url": "/react-calculator/static/css/main.094cb9f4.chunk.css"
+  },
+  {
+    "revision": "ba34fd5455c6ff3cdc71",
+    "url": "/react-calculator/static/js/2.b9568f30.chunk.js"
+  },
+  {
+    "revision": "e3bdb7a8634048fa1163",
+    "url": "/react-calculator/static/js/main.fd51ed13.chunk.js"
+  },
+  {
+    "revision": "e3ec367fb38dabeb7a81",
+    "url": "/react-calculator/static/js/runtime~main.466f139f.js"
+  }
+]);
